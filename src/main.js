ee7f9f24a15a4c36fb13ae5c3e84fa84d142ac70
bakry2002146 <al7166232@gmail.com>
2026-07-18 12:09:15 +0200

    Add File Picker

--- src/main.js
+++ src/main.js
@@ -1,25 +1,43 @@
 import './style.css'
+import { FilePicker } from '@capawesome/capacitor-file-picker';
 
 document.querySelector('#app').innerHTML = `
 <div class="container">
-  <h1>📦 MyAPKBuilder</h1>
+<h1>📦 MyAPKBuilder</h1>
 
-  <div class="card">
-    <h2>اختر المشروع</h2>
-    <button id="select">📂 اختر مجلد المشروع</button>
-  </div>
+<div class="card">
+<h2>اختر المشروع</h2>
 
-  <div class="card">
-    <h2>إعدادات التطبيق</h2>
+<button id="select">📂 اختر ملف ZIP</button>
 
-    <input type="text" placeholder="اسم التطبيق">
-
-    <input type="text" placeholder="Package Name">
-
-    <button id="build">🚀 Build APK</button>
-  </div>
+<p id="result">لم يتم اختيار أي ملف.</p>
 </div>
-`
-document.getElementById("build").onclick = function () {
-  alert("🔥 زر Build APK يعمل!");
-}
+
+<div class="card">
+<h2>إعدادات التطبيق</h2>
+
+<input id="appname" type="text" placeholder="اسم التطبيق">
+
+<input id="package" type="text" placeholder="Package Name">
+
+<button id="build">🚀 Build APK</button>
+</div>
+</div>
+`;document.getElementById("select").addEventListener("click", async () => {
+  try {
+    const result = await FilePicker.pickFiles({
+      types: ["application/zip"]
+    });
+
+    document.getElementById("result").innerText =
+      "✅ " + result.files[0].name;
+
+  } catch (err) {
+    document.getElementById("result").innerText =
+      "❌ لم يتم اختيار أي ملف";
+  }
+});
+
+document.getElementById("build").addEventListener("click", () => {
+  alert("🚀 سيتم بناء APK قريبًا");
+});
